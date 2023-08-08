e6de021b296af4f42bf7667065fd2577cde42e19
xlsak5 <47886477+xlsak5@users.noreply.github.com>
2023-08-08 17:46:30 +0900

    2023.08.08

--- js/javascript.js
+++ js/javascript.js
@@ -1,15 +1,61 @@
-// let mainMenuHover = document.querySelector(".header__bottom .second")
-// let subMenu = document.querySelector(".header__bottom .menu > li > .sub__menu > li");
+ // 선택자 
+ const sliderWrap = document.querySelector(".slider__wrap");
+ const sliderImg = sliderWrap.querySelector(".slider__img");      //보여지는 영역
+ const sliderInner = sliderWrap.querySelector(".slider__inner"); //움직이는 영역
+ const slider = sliderWrap.querySelectorAll(".slider");          //개별 영역
+ const sliderDot = sliderWrap.querySelector(".slider__dot");     //닷 메뉴
+ const sliderBtn = sliderWrap.querySelectorAll(".slider__btn a");//버튼
 
-// subMenu.addEventListener("mouseover", () => {
-//   subMenu.style.display = "block";
-//   console.log("aa");
-// })
-$(function () {
-  $(".menu > ul > li").mouseover(function () {
-      $(this).find(".sub__menu").stop().slideDown(300);
-  });
-  $(".menu > ul > li").mouseout(function () {
-      $(this).find(".sub__menu").stop().slideUp(300);
-  });
-});
+ let currentIndex = 0;                               //현재 보이는 이미지
+ let sliderCount = slider.length;                    //이미지 갯수
+ let sliderInterval = 3000;                          //이미지 간격 시간
+ let sliderWidth = slider[0].offsetWidth;            //이미지 가로값
+ let dotIndex = "";
+
+ function init(){
+     //이미지 갯수 만큼 닷메뉴 생성
+     slider.forEach(() => dotIndex += "<a href='#' class='dot cli'>이미지</a>");
+     sliderDot.innerHTML = dotIndex;
+
+     //첫번째 닷메뉴 활성화 표시
+     sliderDot.firstChild.classList.add("active");
+
+
+     // 닷버튼 클릭
+     let dotClick = sliderWrap.querySelectorAll(".cli");
+     dotClick.forEach((el, index) => {
+         dotClick[index].addEventListener("click", () => {
+             gotoSlider(index);
+         });
+     });
+ }
+ init();
+ 
+ // 이미지 이동시키기
+ function gotoSlider(num){
+    // 400ms : 4초
+    sliderInner.style.transition = "all 100ms";
+    sliderInner.style.transform = "translateX(" + (-sliderWidth*num) +"%)";
+    currentIndex = num;
+
+    // 닷 메뉴 활성화 하기
+    let dotActive = document.querySelectorAll(".slider__dot .dot");
+    dotActive.forEach((active) => { active.classList.remove("active"); });
+    dotActive[num].classList.add("active");
+ }
+
+ // next + prev 버튼을 클릭
+ sliderBtn.forEach((btn, index) => {
+     btn.addEventListener("click", () => {
+         let prevIndex = (currentIndex+(sliderCount-1)) % sliderCount;
+         let nextIndex = (currentIndex+1) % sliderCount;
+
+         if(btn.classList.contains("prev")){
+             gotoSlider(prevIndex);
+         }
+         else {
+             gotoSlider(nextIndex);
+             console.log("asd");
+         }
+     });
+ });
